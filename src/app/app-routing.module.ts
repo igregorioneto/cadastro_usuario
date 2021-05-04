5dc186fb32aa524fafa98f58ae0d2cad53fd3df2
igregorioneto <igregoriodoneto@outlook.com>
2021-05-03 22:52:57 -0300

    Cadastro-Usuario-Frontend- Criando tela de listagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ListaComponent } from './components/usuarios/lista/lista.component';
+import { CriarComponent } from './components/usuarios/criar/criar.component';
+
+const routes: Routes = [
+  {path: '', component: ListaComponent},
+  {path: 'criar', component: CriarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './components/template/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ListaComponent } from './components/usuarios/lista/lista.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CriarComponent } from './components/usuarios/criar/criar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListaComponent,
+    CriarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
